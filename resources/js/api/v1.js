739e189cac736db9ccf466f2b2149d35bbc50a44
Yar Zar Lin Htet <myaungkinn@gmail.com>
2020-06-08 15:59:09 +0630

    fix token add

--- resources/js/api/v1.js
+++ resources/js/api/v1.js
@@ -2,12 +2,13 @@ import axios from 'axios';
 import store from '../store';
 
 const api = axios.create({
-    baseURL: '/api/'
+    baseURL: '/api/',
+    headers: { "Authorization" : `${store.getters.token ? store.getters.token: ''}`}
 });
 
-if(store.getters.token){
-    api.defaults.headers.common['Authorization'] = store.getters.token;
-}
+// if(store.getters.token){
+//     api.defaults.headers.common['Authorization'] = store.getters.token;
+// }
 
 export {
     api,
