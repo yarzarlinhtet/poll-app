d754b580f35a1218356593b4db5033bcb1493dc6
Yar Zar Lin Htet <myaungkinn@gmail.com>
2020-06-08 16:39:13 +0630

    fix

--- resources/js/api/v1.js
+++ resources/js/api/v1.js
@@ -3,12 +3,22 @@ import store from '../store';
 
 const api = axios.create({
     baseURL: '/api/',
-    headers: { "Authorization" : `${store.getters.token ? store.getters.token: ''}`}
+    //headers: { "Authorization" : `${store.getters.token ? store.getters.token: ''}`}
 });
 
 // if(store.getters.token){
 //     api.defaults.headers.common['Authorization'] = store.getters.token;
 // }
+api.interceptors.request.use(function (config) {
+    if(store.getters.token){
+        config.headers.common['Authorization'] = store.getters.token;
+    }
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+
 
 export {
     api,
